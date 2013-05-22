79890d020ed1d7e2f3dab64a5c919c482c75b1b9
AJ Rossman <aj@smartresourceinstitute.com>
2013-05-22 17:42:25 -0400

    check in file exists

--- lib/mission_control_command.rb
+++ lib/mission_control_command.rb
@@ -1,5 +1,4 @@
 class MissionControlCommand
-
   def initialize(cmd)
     @cmd = cmd
   end
@@ -8,14 +7,44 @@ class MissionControlCommand
     ENV['HOME'] + '/.check_in'
   end
 
-  def output
-    if @cmd == 'in'
-      File.open(self.class.check_in_path, 'w') do |f|
-        f.puts 'checkedin'
-      end
-      'You are checked in'
-    else
-      'You are checked out'
+  def self.check_in_file_exists?
+    File.exists?(check_in_path)
+  end
+
+  def check_in
+    File.open(self.class.check_in_path, "w") do |f|
+      f.puts 'checkedin'
     end
   end
+
+  def check_checked_in
+    cco = File.exists?(self.class.check_in_path)
+    puts cco
+    return cco
+  end
+
+  def output
+    output = "no output"
+
+    if @cmd == 'in'
+      check_in
+      output ='You are checked in'
+
+    elsif @cmd == 'out'
+      #check to see if already checked in
+      if(check_checked_in == false)
+        puts 'You are already checked out'
+        output = 'You are already checked out'
+      else
+        File.delete(self.class.check_in_path)
+        output = 'You are checked out'
+       end
+
+    else
+      'You are in limbo'
+    end
+
+    return output
+  end
+
 end
--- spec/mission_control_command_spec.rb
+++ spec/mission_control_command_spec.rb
@@ -1,11 +1,38 @@
 require 'rspec'
+require 'pry'
 
 require_relative '../lib/mission_control_command'
 
 describe MissionControlCommand do
   it 'has a check in path'
 
+  describe "#check_in_file_exists?" do
+    it "returns true if check in file exists and mc" do
+
+      file_path = MissionControlCommand.check_in_path
+      File.open(file_path, "w") do |f|
+        f.puts 'checkedin'
+      end
+
+      expect(MissionControlCommand.check_in_file_exists?).to be_true
+    end
+
+    it "returns false if check in file doesnt exist" do
+
+      file_path = MissionControlCommand.check_in_path
+
+      if FileTest.exists?(file_path)
+        FileUtils.rm(file_path)
+      end
+
+      expect(File.exists?(file_path)).to_not be_true
+
+      expect(MissionControlCommand.check_in_file_exists?).to_not be_true
+    end
+  end
+
   context 'checking in' do
+
     it 'acknowledges my check in' do
       cmd = MissionControlCommand.new('in')
       expect(cmd.output).to include('You are checked in')
@@ -26,10 +53,20 @@ describe MissionControlCommand do
 
 
   context 'checking out' do
+    it 'I get a message that I am already checked out if I was already checked out' do
+      cmd = MissionControlCommand.new('out')
+      cmd = MissionControlCommand.new('out')
+      expect(cmd.output).to include('You are already checked out')
+    end
+
+
     it 'acknowledges my check out' do
+
       cmd = MissionControlCommand.new('out')
       expect(cmd.output).to include('You are checked out')
     end
+
+
 
     # it 'removes file upon checkout' do
     #   cmd = MissionControlCommand.new('out')
@@ -37,6 +74,8 @@ describe MissionControlCommand do
     #   cmd.output
     #   expect(FileTest.exists?(check_in_path)).to_not be_true
     # end
+
+
 
   end
 
